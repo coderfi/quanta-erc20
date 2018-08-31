2b274a3588d1569c07ef1a5a3294e4aebb51dec4
Fairiz Azizi <coderfi@gmail.com>
2018-08-31 16:57:39 -0700

    remove truffle.js file (and rename to truffle-config.js) per ethereum mastery book best practices remove gas config to ease development issues

--- truffle-config.js
+++ truffle-config.js
@@ -1,10 +1,10 @@
 /*
- * NB: since truffle-hdwallet-provider 0.0.5 you must wrap HDWallet providers in a 
+ * NB: since truffle-hdwallet-provider 0.0.5 you must wrap HDWallet providers in a
  * function when declaring them. Failure to do so will cause commands to hang. ex:
  * ```
  * mainnet: {
- *     provider: function() { 
- *       return new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/<infura-key>') 
+ *     provider: function() {
+ *       return new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/<infura-key>')
  *     },
  *     network_id: '1',
  *     gas: 4500000,
@@ -12,7 +12,28 @@
  *   },
  */
 
+var LedgerWalletProvider = require("truffle-ledger-provider");
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var mnemonic = "foil wisdom culture upon response early volcano rhythm enable glimpse kite alone";
+
+var infura_apikey = 'f884e325628b4c58aaa49de658a5a753';
+
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+  networks: {
+    development: {
+      host: "localhost",
+      port: 7545,
+      network_id: "*" // Match any network id,
+    },
+    ropsten: {
+      provider: new HDWalletProvider(mnemonic, "https://ropsten.infura.io/" + infura_apikey),
+      network_id: 3,
+      gas: 4600000
+    },
+    live: {
+      host: "localhost",
+      port: 8546,
+      network_id: 1 // Ethereum public network
+    }
+  }
 };
