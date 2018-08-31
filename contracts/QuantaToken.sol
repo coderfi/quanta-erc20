4ea48f6c86a1fbff901da4618223019eaff8e664
Fairiz Azizi <coderfi@gmail.com>
2018-08-31 16:42:48 -0700

    fix contracts/QuantaToken.sol per Solium solidity linter recommendations
        $ npm install -g solium
        $ solium -d contracts
        contracts/QuantaToken.sol
          3:7     error      "./zeppelin/token/PausableToken.sol": Import statements must use double quotes only.    quotes
          11:4    error      No visibility specified explicitly for QuantaToken function.                            security/enforce-explicit-visibility
          21:4    error      No visibility specified explicitly for changeAdmin function.                            security/enforce-explicit-visibility
          23:8    warning    Use emit statements for triggering events.                                              emit

--- contracts/QuantaToken.sol
+++ contracts/QuantaToken.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.4.18;
 
-import './zeppelin/token/PausableToken.sol';
+import "./zeppelin/token/PausableToken.sol";
 
 contract QuantaToken is PausableToken {
     string  public  constant name = "QUANTA";
@@ -8,19 +8,19 @@ contract QuantaToken is PausableToken {
     uint8   public  constant decimals = 9;
     uint256 public INITIAL_SUPPLY = 400000000 * (10 ** uint256(decimals));
 
-    function QuantaToken() 
+    constructor() public
     {
         admin = owner;
         totalSupply = INITIAL_SUPPLY;
         balances[msg.sender] = INITIAL_SUPPLY;
-        Transfer(address(0x0), msg.sender, INITIAL_SUPPLY);
+        emit Transfer(address(0x0), msg.sender, INITIAL_SUPPLY);
     }
 
     event AdminTransferred(address indexed previousAdmin, address indexed newAdmin);
 
-    function changeAdmin(address newAdmin) onlyOwner {
+    function changeAdmin(address newAdmin) public onlyOwner {
         // owner can re-assign the admin
-        AdminTransferred(admin, newAdmin);
+        emit AdminTransferred(admin, newAdmin);
         admin = newAdmin;
     }
 }
